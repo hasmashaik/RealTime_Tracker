dde48af2e6509ba2c5a36f7161bc17ace5e9ed76
Hasma Shaik <hasma200114@gmail.com>
2026-01-29 19:14:19 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket=io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const{latitude,longitude}= position.coords;
+        socket.emit("send-location",{latitude,longitude});
+    },(error)=>{
+console.log(error)
+    },
+{
+    enableHighAccuracy:true,
+    timeout:5000,
+    maximumAge:0
+}
+);
+}
+
+const map=L.map("map").setView([0,0],16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: "Sheryiens Map"
+}).addTo(map);            //open street map layer
+
+const marker={};
+
+socket.on("recive-location",(data)=>{
+    const {id,latitude,longitude}=data;
+    map.setView([latitude,longitude]);
+
+    if(marker[id]){
+        marker[id].setLatLng([latitude,longitude]);
+    }
+    else{
+        marker[id]=L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected",(id)=>{
+    if(marker[id]){
+        map.removeLayer(marker[id]);
+        delete marker[id];
+    }   
+});
